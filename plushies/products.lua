87c3f171a14b7c5f4f60d292f3adb74bb7e1eb6d
Emily (Selim) <042Selim@gmail.com>
2023-08-13 14:06:33 -0400

    Create products.lua

---
+++ plushies/products.lua
@@ -0,0 +1,102 @@
+return {
+  {
+    modid = "sc-peripherals:print",
+    id = 1,
+    hidden = false,
+    quantity = 62,
+    name = "Smol Penguin",
+    category = "Peng",
+    price = 3,
+    __opaque = true,
+    predicates = {
+      displayName = "Smol Penguin",
+    },
+    address = "s-peng",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 3,
+    hidden = false,
+    quantity = 62,
+    name = "Wide Penguin",
+    category = "Peng",
+    __opaque = true,
+    id = 2,
+    predicates = {
+      displayName = "Wide Penguin",
+    },
+    address = "w-peng",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 3,
+    __opaque = true,
+    quantity = 62,
+    name = "Non-Beenairy",
+    address = "nb-bee",
+    category = "Bee",
+    predicates = {
+      displayName = "Non-Beenairy",
+    },
+    id = 1,
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 3,
+    __opaque = true,
+    id = 2,
+    quantity = 62,
+    name = "Bee-Valid",
+    category = "Bee",
+    predicates = {
+      displayName = "Bee-Valid",
+    },
+    address = "tr-bee",
+  },
+  {
+    modid = "sc-peripherals:print",
+    id = 3,
+    __opaque = true,
+    quantity = 62,
+    name = "Gay Penguin",
+    address = "g-peng",
+    price = 3,
+    predicates = {
+      displayName = "Gay Penguin",
+    },
+    category = "Peng",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 3,
+    __opaque = true,
+    name = "Duck",
+    category = "Misc",
+    id = 1,
+    quantity = 62,
+    predicates = {
+      displayName = "Duck",
+    },
+    address = "duck",
+  },
+  {
+    predicates={
+      displayName="Baby Phantom"
+    },
+    modid = "sc-peripherals:print",
+    name = "Baby Phantom",
+    category = "Phant",
+    price = 3,
+    address = "bb-phant",
+  },
+  {
+    predicates={
+      displayName="Trans Phantom"
+    },
+    modid = "sc-peripherals:print",
+    name = "Trans Phantom",
+    category = "Phant",
+    price = 3,
+    address = "tr-phant",
+  },
+}

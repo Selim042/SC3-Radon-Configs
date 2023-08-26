be3fb98359b2e3ee509b6e001ea33018c82170fe
Emily (Selim) <042Selim@gmail.com>
2023-08-26 11:25:29 -0400

    Create products.lua

---
+++ pokemon/products.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    modid = "sc-peripherals:print",
+    name = "Swablu",
+    price = 5,
+    predicates = {
+      displayName = "Swablu",
+    },
+    address = "swablu",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 1000,
+    hidden = true,
+    name = "Shiny Swablu",
+    predicates = {
+      displayName = "Shiny Swablu",
+    },
+    address = "shiny-swablu",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    name = "Wingull",
+    price = 5,
+    predicates = {
+      displayName = "Wingull",
+    },
+    address = "wingull",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 1000,
+    hidden = true,
+    name = "Shiny Wingull",
+    predicates = {
+      displayName = "Shiny Wingull",
+    },
+    address = "shiny-wingull",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    name = "Chatot",
+    price = 5,
+    predicates = {
+      displayName = "Chatot",
+    },
+    address = "chatot",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 1000,
+    hidden = true,
+    name = "Shiny Chatot",
+    predicates = {
+      displayName = "Shiny Chatot",
+    },
+    address = "shiny-chatot",
+    designer = "Entropy_44",
+  },
+  {
+    modid = "sc-peripherals:print",
+    price = 1000,
+    hidden = true,
+    name = "Shiny Munchlax",
+    predicates = {
+      displayName = "Shiny Munchlax",
+    },
+    address = "shiny-munchlax",
+    designer = "Entropy_44",
+  },
+}

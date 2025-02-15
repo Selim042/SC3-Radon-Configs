b686854f1bd655511a6954c8d9dded8287e4fbd7
Emily (Selim) <042Selim@gmail.com>
2025-02-15 15:48:34 -0500

    Create products.lua

---
+++ enchants/products.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    modid = "minecraft:enchanted_book",
+    price = 50,
+    __opaque = true,
+    name = "Mending",
+    address = "mending",
+    category = "All",
+    id = 1,
+    quantity = 4,
+    predicates = {
+      enchantments = {
+        {displayName = 'Mending'},
+      },
+    },
+  },
+  {
+    modid = "minecraft:enchanted_book",
+    price = 50,
+    __opaque = true,
+    name = "Looting III",
+    address = "looting",
+    category = "All",
+    id = 1,
+    quantity = 4,
+    predicates = {
+      enchantments = {
+        {displayName = 'Looting III'},
+      },
+    },
+  },
+}
